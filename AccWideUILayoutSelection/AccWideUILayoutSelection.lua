f9be846bd0cc6c1a28cca5cea2f2538cb4d683ac
NinerBull <72740816+NinerBull@users.noreply.github.com>
2022-11-09 23:35:27 +0100

    Initial Upload

---
+++ AccWideUILayoutSelection/AccWideUILayoutSelection.lua
@@ -0,0 +1,276 @@
+local AccWideUIFrame = CreateFrame("Frame")
+AccWideUIFrame:RegisterEvent("ADDON_LOADED")
+AccWideUIFrame:RegisterEvent("PLAYER_LOGIN")
+AccWideUIFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+AccWideUIFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+AccWideUIFrame:RegisterEvent("EDIT_MODE_LAYOUTS_UPDATED")
+
+
+hooksecurefunc(C_EditMode, "OnEditModeExit", function()
+        
+	
+	getLayoutsTable = C_EditMode.GetLayouts()
+	currentActiveLayout = getLayoutsTable["activeLayout"]
+	currentSpec = GetSpecialization()
+	
+
+	
+	
+	if (AccWideUIDataChar[currentSpec] == true) then
+
+		--Set the spec
+		AccWideUIData.accountWideLayoutID = currentActiveLayout
+		
+		if (AccWideUIData.enableTextOutput == true) then
+			print("|cffdb562a<AccountWideUI>:|r Saved your Account Wide UI to the selected Layout. (ID " .. AccWideUIData.accountWideLayoutID .. "). Type |cffdb562a/accwideui|r to configure.")
+		end
+
+	end
+
+		
+end)
+
+AccWideUIFrame:SetScript("OnEvent", function(self, event, arg1, arg2)
+
+	if (event == "PLAYER_LOGIN") then
+	
+	
+		getLayoutsTable = C_EditMode.GetLayouts()
+		currentActiveLayout = getLayoutsTable["activeLayout"]
+
+		if (AccWideUIData == nil) then
+	
+			AccWideUIData = {
+				enableAccountWide = true,
+				accountWideLayoutID = currentActiveLayout or 1,
+				enableTextOutput = true
+			}
+		
+		end
+		
+		
+		
+		if (AccWideUIDataChar == nil) then
+	
+			AccWideUIDataChar = {
+					[1] = AccWideUIData.enableAccountWide,
+					[2] = AccWideUIData.enableAccountWide,
+					[3] = AccWideUIData.enableAccountWide,
+					[4] = AccWideUIData.enableAccountWide,
+					[5] = AccWideUIData.enableAccountWide --Temp Spec for fresh chars, adding just in case
+			}
+		
+			
+			print("|cffdb562a<AccountWideUI>:|r This is the first time you have logged in to this character with AccountWideUI installed.")
+			
+			if (AccWideUIData.enableAccountWide == true) then
+				print("|cffdb562a<AccountWideUI>:|r Account Wide UI has been ENABLED by default. Type |cffdb562a/accwideui|r to configure.")
+			else
+				print("|cffdb562a<AccountWideUI>:|r Account Wide UI has been DISABLED by default. Type |cffdb562a/accwideui|r to configure.")
+			end
+		
+		
+		else
+		
+			print("|cffdb562a<AccountWideUI>:|r Account Wide UI Addon Loaded! Type |cffdb562a/accwideui|r to configure.")
+		
+		end
+		
+		AccWideUIFrame.InitializeOptions()
+	
+	end
+
+
+
+--(event == "ADDON_LOADED" and arg1 == "AccWideUIFrameLayoutSelection") or
+    if  (event == "PLAYER_LOGIN") or (event == "PLAYER_SPECIALIZATION_CHANGED") then
+
+		
+		getLayoutsTable = C_EditMode.GetLayouts()
+		currentActiveLayout = getLayoutsTable["activeLayout"]
+		currentSpec = GetSpecialization()
+		
+		if (AccWideUIDataChar[currentSpec] == true) then
+			
+
+			--Set the spec
+			C_EditMode.SetActiveLayout(AccWideUIData.accountWideLayoutID)
+			
+			if (AccWideUIData.enableTextOutput == true) then
+				print("|cffdb562a<AccountWideUI>:|r Changed UI Layout for this Specialization to your Account Wide UI. (ID " .. AccWideUIData.accountWideLayoutID .. "). Type |cffdb562a/accwideui|r to configure.")
+			end
+			
+		
+		--else
+	
+		end
+
+		    
+    end
+
+end)
+
+
+function AccWideUIFrame:InitializeOptions()
+	
+		accWideUIPanel = CreateFrame("Frame")
+		accWideUIPanel.name = "Account Wide UI Layout Selection"
+		InterfaceOptions_AddCategory(accWideUIPanel) 
+
+		--Title
+		local title = accWideUIPanel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
+		title:SetJustifyV('TOP')
+		title:SetJustifyH('LEFT')
+		title:SetPoint('TOPLEFT', 16, -16)
+		title:SetText("Account Wide UI Layout Selection")
+		
+		--Title2
+		local title2 = accWideUIPanel:CreateFontString("ARTWORK", nil, "GameFontHighlight")
+		title2:SetJustifyV('TOP')
+		title2:SetJustifyH('LEFT')
+		title2:SetPoint('TOPLEFT', 16, -36)
+		title2:SetText("Makes your chosen Edit Mode Layout apply for all of your characters and specs.")
+		
+		
+		
+		
+		
+		--Enable by Default
+		local chkEnableDefault = CreateFrame("CheckButton", nil, accWideUIPanel, "InterfaceOptionsCheckButtonTemplate")
+		chkEnableDefault:SetPoint("TOPLEFT", 16, -60)
+		chkEnableDefault.Text:SetText("Enable by default for all New Characters")
+		chkEnableDefault:HookScript("OnClick", function(_, btn, down)
+			AccWideUIData.enableAccountWide = chkEnableDefault:GetChecked()
+		end)
+		chkEnableDefault:SetChecked(AccWideUIData.enableAccountWide)
+		
+		
+		-- Show Text
+		local chkShowText = CreateFrame("CheckButton", nil, accWideUIPanel, "InterfaceOptionsCheckButtonTemplate")
+		chkShowText:SetPoint("TOPLEFT", 16, -85)
+		chkShowText.Text:SetText("Output to chat when UI is changed by Account Wide UI")
+		chkShowText:HookScript("OnClick", function(_, btn, down)
+				AccWideUIData.enableTextOutput = chkShowText:GetChecked()
+		end)
+		chkShowText:SetChecked(AccWideUIData.enableTextOutput)
+
+	
+	
+
+		
+		
+		
+		csPlayerName = UnitName("player")
+		
+		--Title for Char Specific
+		local titleCS = accWideUIPanel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
+		titleCS:SetJustifyV('TOP')
+		titleCS:SetJustifyH('LEFT')
+		titleCS:SetPoint('TOPLEFT', 16, -160)
+		titleCS:SetText(csPlayerName .. " Specific Options")
+		
+		--Title for Char Specific2
+		local titleCS2 = accWideUIPanel:CreateFontString("ARTWORK", nil, "GameFontHighlight")
+		titleCS2:SetJustifyV('TOP')
+		titleCS2:SetJustifyH('LEFT')
+		titleCS2:SetPoint('TOPLEFT', 16, -182)
+		titleCS2:SetText("Select whether to use the Account Wide UI for this character's Specializations.")
+		
+		
+		
+		
+		
+		
+	
+		--Specialisations
+		specName = {}
+		numSpecializations = GetNumSpecializations(false, false)
+	
+		for specx = 1, numSpecializations, 1 do
+		
+			specName[specx] = GetSpecializationNameForSpecID(select(1, GetSpecializationInfo(specx)))
+		
+		end
+	
+		--tprint(specName)
+		
+		
+		if (numSpecializations >= 1) then
+		
+			local chkEnableSpec1 = CreateFrame("CheckButton", nil, accWideUIPanel, "InterfaceOptionsCheckButtonTemplate")
+			chkEnableSpec1:SetPoint("TOPLEFT", 16, -200)
+			chkEnableSpec1.Text:SetText(specName[1])
+			chkEnableSpec1:HookScript("OnClick", function(_, btn, down)
+				AccWideUIDataChar[1] = chkEnableSpec1:GetChecked()
+			end)
+			chkEnableSpec1:SetChecked(AccWideUIDataChar[1])
+
+		end
+		
+		if (numSpecializations >= 2) then
+		
+			local chkEnableSpec2 = CreateFrame("CheckButton", nil, accWideUIPanel, "InterfaceOptionsCheckButtonTemplate")
+			chkEnableSpec2:SetPoint("TOPLEFT", 16, -222)
+			chkEnableSpec2.Text:SetText(specName[2])
+			chkEnableSpec2:HookScript("OnClick", function(_, btn, down)
+				AccWideUIDataChar[2] = chkEnableSpec2:GetChecked()
+			end)
+			chkEnableSpec2:SetChecked(AccWideUIDataChar[2])
+
+		end
+		
+		if (numSpecializations >= 3) then
+		
+			local chkEnableSpec3 = CreateFrame("CheckButton", nil, accWideUIPanel, "InterfaceOptionsCheckButtonTemplate")
+			chkEnableSpec3:SetPoint("TOPLEFT", 16, -244)
+			chkEnableSpec3.Text:SetText(specName[3])
+			chkEnableSpec3:HookScript("OnClick", function(_, btn, down)
+				AccWideUIDataChar[3] = chkEnableSpec3:GetChecked()
+			end)
+			chkEnableSpec3:SetChecked(AccWideUIDataChar[3])
+
+		end
+		
+		if (numSpecializations >= 4) then
+		
+			local chkEnableSpec4 = CreateFrame("CheckButton", nil, accWideUIPanel, "InterfaceOptionsCheckButtonTemplate")
+			chkEnableSpec4:SetPoint("TOPLEFT", 16, -266)
+			chkEnableSpec4.Text:SetText(specName[4])
+			chkEnableSpec4:HookScript("OnClick", function(_, btn, down)
+				AccWideUIDataChar[4] = chkEnableSpec4:GetChecked()
+			end)
+			chkEnableSpec4:SetChecked(AccWideUIDataChar[4])
+
+		end
+		
+		if (numSpecializations >= 5) then
+		
+			local chkEnableSpec5 = CreateFrame("CheckButton", nil, accWideUIPanel, "InterfaceOptionsCheckButtonTemplate")
+			chkEnableSpec5:SetPoint("TOPLEFT", 16, -288)
+			chkEnableSpec5.Text:SetText(specName[5])
+			chkEnableSpec5:HookScript("OnClick", function(_, btn, down)
+				AccWideUIDataChar[5] = chkEnableSpec5:GetChecked()
+			end)
+			chkEnableSpec5:SetChecked(AccWideUIDataChar[5])
+
+		end
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		SLASH_ACCWIDEUI1 = "/accwideui"
+		function SlashCmdList.ACCWIDEUI(msg)
+			InterfaceOptionsFrame_OpenToCategory(accWideUIPanel)
+			InterfaceOptionsFrame_OpenToCategory(accWideUIPanel)
+		end
+	
+end
+
+
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# Account Wide UI Layout Selection
+
+This addon allows you to use a UI layout that you've created with the Edit Mode in World of Warcraft, and automatically apply it to all of your characters and specs. By default, every character and spec has to have their UI chosen individually.
+
+To do this, simply install the addon, and choose your Edit Mode UI in the Edit Mode. Your selected UI Layout is saved and will be used for all your chars!
+
+You can configure the addon by typing `/accwideui` into chat.
+
+<ul>
+<li>You can toggle whether the Account Wide UI applies automatically to all new characters (or chars you haven't yet logged into after installing the addon).</li>
+<li>You can toggle whether text is written into chat when the Addon sets your UI Layout.</li>
+<li>You can toggle whether the Account Wide UI is used on any or all specs. You'll need to do this for every character.</li>
+</ul>
