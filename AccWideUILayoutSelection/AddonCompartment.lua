6792b59c78bdf0682572d1476868c2bf94f0787a
NinerBull <72740816+NinerBull@users.noreply.github.com>
2026-01-05 23:56:33 +0100

    Update Addon Compartment

--- AccWideUILayoutSelection/AddonCompartment.lua
+++ AccWideUILayoutSelection/AddonCompartment.lua
@@ -1,11 +1,12 @@
 local L = LibStub("AceLocale-3.0"):GetLocale("AccWideUIAceAddonLocale")
 AccWideUIAceAddon.LDBIcon = LibStub:GetLibrary("LibDBIcon-1.0")
 
+
 -- LDB for Titan Panel
 AccWideUIAceAddon.LDB = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject(L["ACCWUI_ADDONNAME_SHORT"], {  
 	type = "data source",  
 	text = L["ACCWUI_ADDONNAME_SHORT"],  
-	icon = "interface\\icons\\inv_misc_punchcards_yellow.blp",  
+	icon = C_AddOns.GetAddOnMetadata("AccWideUILayoutSelection", "IconTexture"), 
 	OnClick = function() AccWideUI_CompartmentClick() end,
 	OnTooltipShow = function(tooltip)
 		if (AccWideUIAceAddon.db.global.hasDoneFirstTimeSetup == true) then
@@ -21,45 +22,49 @@ AccWideUIAceAddon.LDB = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject(L[
 });
 
 
+-- Addon Compartment
+if (AddonCompartmentFrame) then
 
---Addon Compartment
-local AccWideUI_Tooltip
+	AddonCompartmentFrame:RegisterAddon({
+		text = L["ACCWUI_ADDONNAME_SHORT"],
+		icon = C_AddOns.GetAddOnMetadata("AccWideUILayoutSelection", "IconTexture"),
+		notCheckable = true,
+		
+		func = function(button)
+			if (AccWideUIAceAddon.db.global.hasDoneFirstTimeSetup == true) then
+				AccWideUIAceAddon:SlashCommand()
+			else
+				StaticPopup_Show("ACCWIDEUI_FIRSTTIMEPOPUP")
+			end
+		end,
+		
+		funcOnEnter = function(button)
+			if (AccWideUIAceAddon.db.global.hasDoneFirstTimeSetup == true) then
 
-function AccWideUI_CompartmentClick(addonName, buttonName)
-	if (AccWideUIAceAddon.db.global.hasDoneFirstTimeSetup == true) then
-		AccWideUIAceAddon:SlashCommand()
-	else
-		StaticPopup_Show("ACCWIDEUI_FIRSTTIMEPOPUP")
-	end
+				if (not AccWideUIAceAddon.Tooltip) then
+					AccWideUIAceAddon.Tooltip = CreateFrame("GameTooltip", "AccWideUIAceAddon.Tooltip_Compartment", UIParent, "GameTooltipTemplate")
+				end
+				
+				AccWideUIAceAddon.Tooltip:SetOwner(button, "ANCHOR_LEFT");
+				
+				AccWideUIAceAddon.Tooltip:SetText(L.ACCWUI_ADDONNAME)
+				
+				AccWideUIAceAddon.Tooltip:AddLine(" ")
+				AccWideUIAceAddon.Tooltip:AddDoubleLine(L["ACCWUI_ADCOM_CURRENT"] .. ":", AccWideUIAceAddon.db:GetCurrentProfile(), nil, nil, nil,  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+				
+				AccWideUIAceAddon.Tooltip:AddLine(" ")
+				AccWideUIAceAddon.Tooltip:AddLine(L.ACCWUI_ADCOM_CHANGE,  GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b)
+				
+				AccWideUIAceAddon.Tooltip:Show()
+			
+			end
+		end,
+		
+		funcOnLeave = function(button)
+			if (AccWideUIAceAddon.db.global.hasDoneFirstTimeSetup == true) then
+				AccWideUIAceAddon.Tooltip:Hide()
+			end
+		end,
+	})
+
 end
-
-function AccWideUI_CompartmentHover(addonName, buttonName)
-
-	if (AccWideUIAceAddon.db.global.hasDoneFirstTimeSetup == true) then
-
-		if (not AccWideUI_Tooltip) then
-			AccWideUI_Tooltip = CreateFrame("GameTooltip", "AccWideUI_Tooltip_Compartment", UIParent, "GameTooltipTemplate")
-		end
-		
-		AccWideUI_Tooltip:SetOwner(buttonName, "ANCHOR_LEFT");
-		
-		AccWideUI_Tooltip:SetText(L.ACCWUI_ADDONNAME)
-		
-		AccWideUI_Tooltip:AddLine(" ")
-		AccWideUI_Tooltip:AddDoubleLine(L["ACCWUI_ADCOM_CURRENT"] .. ":", AccWideUIAceAddon.db:GetCurrentProfile(), nil, nil, nil,  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
-		
-		AccWideUI_Tooltip:AddLine(" ")
-		AccWideUI_Tooltip:AddLine(L.ACCWUI_ADCOM_CHANGE,  GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b)
-		
-		AccWideUI_Tooltip:Show()
-	
-	end
-	
-end
-
-function AccWideUI_CompartmentLeave(buttonName)
-	if (AccWideUIAceAddon.db.global.hasDoneFirstTimeSetup == true) then
-		AccWideUI_Tooltip:Hide()
-	end
-end
-
