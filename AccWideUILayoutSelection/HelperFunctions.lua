9d2889c5b7848049a5954fe615b795d8ce6811dd
NinerBull <72740816+NinerBull@users.noreply.github.com>
2026-07-05 00:01:38 +0200

    Major Feature Detection Refactor
    Check per feature rather than disjointed client detection all over the place

--- AccWideUILayoutSelection/HelperFunctions.lua
+++ AccWideUILayoutSelection/HelperFunctions.lua
@@ -36,6 +36,34 @@ function AccWideUIAceAddon:IsClassicEra()
 	return (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC) or false
 end
 
-function AccWideUIAceAddon:SupportsEditMode()
-	return (self:IsMainline() or self:IsClassicTBC() or self:IsClassicProgression())
+function AccWideUIAceAddon:SupportsGameFunction(functionName)
+	-- Should return True if the game supports a particular function and therefore can be synced. 
+	-- Only things that are not in all clients (e.g. Arena) should be listed here.
+	
+	if (functionName == "editModeLayout") then -- Edit Mode
+		return (self:IsMainline() or self:IsClassicWrath() or self:IsClassicTBC() or self:IsClassicProgression())
+	elseif (functionName == "lossOfControl") then -- Loss of Control Banners
+		return (self:IsMainline() or self:IsClassicWrath() or self:IsClassicTBC() or self:IsClassicProgression())
+	elseif (functionName == "mouseoverCast") then -- Mouseover Cast
+		return (self:IsMainline() or self:IsClassicWrath() or self:IsClassicTBC() or self:IsClassicProgression())
+	elseif (functionName == "arenaFrames") then -- Arena Frames
+		return (not self:IsClassicEra())
+	elseif (functionName == "spellOverlay") then -- Spell Overlay
+		return (not self:IsClassicEra())
+	elseif (functionName == "empowerTap") then -- Empower Tap
+		return (self:IsMainline())
+	elseif (functionName == "assistedCombat") then -- 1 Button Assist
+		return (self:IsMainline())
+	elseif (functionName == "locationVisibility") then -- Location Visibility Toggle
+		return (self:IsMainline())
+	elseif (functionName == "blockNeighborhoodInvites") then -- Block Neighborhood Invites
+		return (self:IsMainline())
+	elseif (functionName == "bagOrganisation") then -- Bag Organisation
+		return (self:IsMainline())
+	elseif (functionName == "nameplateSizeFunc") then -- New Nameplate Size Func
+		return (self:IsMainline())
+	else
+		return true
+	end
+
 end
